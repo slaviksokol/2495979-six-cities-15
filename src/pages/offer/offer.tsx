02db02199d934e633886d90cf7dbbe56da6939dd
Степин Вячеслав Сергеевич <vstepin@ibs.ru>
2024-02-18 17:10:45 +0300

    2.16. Начинаем программировать. Фикс багов

--- src/pages/offer/offer.tsx
+++ src/pages/offer/offer.tsx
@@ -247,7 +247,7 @@ function Offer(): React.JSX.Element {
                       To submit review please make sure to set <span className="reviews__star">rating</span> and
                       describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
                     </p>
-                    <button className="reviews__submit form__submit button" type="submit" disabled="">Submit</button>
+                    <button className="reviews__submit form__submit button" type="submit" disabled>Submit</button>
                   </div>
                 </form>
               </section>
