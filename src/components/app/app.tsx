0e23261561932f9207bbcebe1e332378f8b8afcf
Степин Вячеслав Сергеевич <vstepin@ibs.ru>
2024-02-18 16:57:50 +0300

    2.16. Начинаем программировать

---
+++ src/components/app/app.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainPage from '../../pages/main/main';
+
+type Offers = {
+  count: number;
+};
+
+export function App({count}: Offers) {
+  return (
+    <MainPage count={count}/>
+  );
+}
+
+export default App;
